158d60f8c11c268e38bebc56ae352589e8515e28
Usman Afzal <usman_9@outlook.com>
2021-07-25 21:58:07 +0500

    Remove method for author has been added

--- embedding.js
+++ embedding.js
@@ -17,14 +17,14 @@ const Course = mongoose.model(
   "Course",
   new mongoose.Schema({
     name: String,
-    author: authorSchema,
+    authors: [authorSchema],
   })
 );
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name,
-    author,
+    authors,
   });
 
   const result = await course.save();
@@ -42,6 +42,22 @@ async function updateAuthor(courseID) {
   course.save();
 }
 
-// createCourse("Node Course", new Author({ name: "Mosh" }));
+async function addAuthor(courseID, author) {
+  const course = await Course.findById(courseID);
+  course.authors.push(author);
+  course.save();
+}
 
-updateAuthor("60fd7db54206de2f9c32eab4");
+async function removeAuthor(courseID, authorID) {
+  const course = await Course.findById(courseID);
+  const author = course.authors.id(authorID);
+  author.remove();
+  course.save();
+}
+
+// createCourse("Node Course", [
+//   new Author({ name: "Mosh" }),
+//   new Author({ name: "Usman" }),
+// ]);
+// updateAuthor("60fd7db54206de2f9c32eab4");
+removeAuthor("60fd8f96bdfbce32a0b6e502", "60fd95d0e079983128c63b3e");
